e09df9630cfee57cbbf9dad03c27b61871d1538a
tolga iskender <tolgaiskender@tolgas-mac.local>
2018-07-28 04:16:10 -0700

    Initial Commit

---
+++ instagramClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instagramClone
+//
+//  Created by tolga iskender on 28.07.2018.
+//  Copyright © 2018 tolga iskender. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
